60f080f291816ac36b90a347f932a11ca33a082f
vchi2333 <vincent.chin@sydney.edu.au>
2020-05-17 01:29:56 +1000

    script updates

--- LANL.R
+++ LANL.R
@@ -1,169 +1,55 @@
+###############################################################################################################
+# Script for standardising LANL prediction data
+###############################################################################################################
+
 rm(list = ls())
 
 # Installing required packages
-packages = c("countrycode", "plyr", "dplyr", "downloader")
-install.packages(packages)
+packages = c("countrycode", "plyr", "dplyr")
+if(!all(packages %in% rownames(installed.packages()))){
+  install.packages(packages[!packages %in% rownames(installed.packages())])
+}
 lapply(packages, require, character.only = T)
 
-# Path to working directory
+# Path to directory
 wkdir = "~/Code/covid-19"
 
-# Model name
-model = c("LANL_global_inc", "LANL_states_inc")
-
-for(i in 1:length(model)){
-# Set working directory
-setwd(paste0(wkdir, "/", model[i]))
+# Name of model and set working directory
+model = "LANL"
+setwd(paste0(wkdir, "/", model))
 
 # Ground truth source
 gt_source = "JHU"
 
-# Load all csv files in the model folder and combine all data
-csv_file             = list.files(pattern=paste0("^", model[i], "(.*)csv$"))
-prediction           = do.call(rbind.fill, lapply(1:length(csv_file), function(i) read.csv(csv_file[i])))
-prediction$dates     = as.Date(prediction$dates)
-prediction$fcst_date = as.Date(prediction$fcst_date)
-prediction           = prediction[prediction$dates > prediction$fcst_date,]
+# Load csv files for the model, combine and convert into standardised format
+summary_global = ldply(list.files(pattern=paste0("^", model, "_global_inc", "(.*)csv$")), function(i) read.csv(i, stringsAsFactors=F, check.names=F))
+summary_global = summary_global %>%
+                 dplyr::mutate(dates=as.Date(dates), fcst_date=as.Date(fcst_date), lookahead=dates-fcst_date, model_name=model,
+                        countries=countrycode(countries, origin="country.name", destination="iso3c", nomatch=NULL), prediction_type="full_perc",
+                        countries=gsub("Kosovo", "KSV", countries), expected_value=q.50, gt_source=gt_source) %>%
+                 rename_at(vars(c("dates", "countries", "fcst_date", paste0("q.", c("01", "025", "05", seq(10, 95, 5), "975", "99")))), 
+                           ~ c("target_end_date", "location_short", "forecast_date", paste0("perc_0.", formatC(c(10, 25, seq(50, 950, 50), 975, 990), width=3, flag="0")))) %>%
+                 filter(lookahead>0)
+summary_global = summary_global[,c(1,29:31,27,32,33,3:25,34)]
 
-# Rename column
-if (model[i] == "LANL_global_inc"){
-  names(prediction)[names(prediction) == "countries"] = "country"
-}else{
-  names(prediction)[names(prediction) == "state"] = "Province_State"
-}
 
-if (model[i] == "LANL_global_inc"){
-  print("Downloading ground truth global data from JHU")
-  download("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
-           "jhu_truth.csv", quiet=T)
-  jhu_truth = read.csv("jhu_truth.csv")
-  jhu_truth = jhu_truth[jhu_truth$Province.State!="Recovered",]
-  jhu_truth = aggregate(.~Country.Region, jhu_truth, sum)
-  cum_death = jhu_truth[,5:ncol(jhu_truth)]
-  inc_death = apply(cum_death, 1, diff)
-  if (all(inc_death>=0)==F){
-    warning("Negative incident deaths reported in ground truth data!")
-  }
-  jhu_truth = data.frame(country=rep(jhu_truth$Country.Region, each=nrow(inc_death)), 
-                         dates=rep(seq(as.Date("2020-01-23"), as.Date("2020-01-23") + nrow(inc_death) - 1, by="days"), ncol(inc_death)),
-                         incident.death=as.vector(inc_death))
-  
-  # Merging prediction with ground truth
-  all_merge = join_all(list(prediction, jhu_truth), by=c("dates", "country"))
-  
-  # Location short and long. Manually adding Kosovo
-  location_short = countrycode(all_merge$country, origin ="country.name", destination="iso3c")
-  location_short[all_merge$country=="Kosovo"] = "KSV"
-  location_long = countrycode(location_short, origin="iso3c", destination="country.name")
-  location_long[is.na(location_long)] = "Kosovo"
-}else{
-  print("Downloading ground truth US states data from JHU")
-  download("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv",
-           "jhu_truth.csv", quiet=T)
-  jhu_truth = read.csv("jhu_truth.csv")
-  jhu_truth = jhu_truth[jhu_truth$Lat!=0 | jhu_truth$Long_!=0,]
-  jhu_truth = aggregate(.~Province_State, jhu_truth, sum)
-  cum_death = jhu_truth[,13:ncol(jhu_truth)]
-  inc_death = apply(cum_death, 1, diff)
-  if (all(inc_death>=0)==F){
-    warning("Negative incident deaths reported in ground truth data!")
-  }
-  jhu_truth = data.frame(Province_State=rep(jhu_truth$Province_State, each=nrow(inc_death)), 
-                         dates=rep(seq(as.Date("2020-01-23"), as.Date("2020-01-23") + nrow(inc_death) - 1, by="days"), ncol(inc_death)),
-                         incident.death=as.vector(inc_death))
-  
-  # Merging prediction with ground truth
-  all_merge = join_all(list(prediction, jhu_truth), by=c("dates", "Province_State"))
-  
-  # Location short and long. Changing Virgins Islands to U.S. Virgin Islands
-  levels(all_merge$Province_State)[levels(all_merge$Province_State)=="Virgin Islands"] <- "U.S. Virgin Islands"
-  location_short = mapvalues(all_merge$Province_State, from=c(state.name, "District of Columbia", "Puerto Rico", "U.S. Virgin Islands"), to=c(state.abb, "DC", "PRI", "VIR"))
-  location_long  = all_merge$Province_State
-}
+summary_us = ldply(list.files(pattern=paste0("^", model, "_states_inc", "(.*)csv$")), function(i) read.csv(i, stringsAsFactors=F, check.names=F))
+summary_us = summary_us %>%
+             dplyr::mutate(dates=as.Date(dates), fcst_date=as.Date(fcst_date), lookahead=dates-fcst_date, model_name=model,
+                           state=mapvalues(state, from=c(state.name, "District of Columbia", "Puerto Rico", "Virgin Islands"), to=c(state.abb, "DC", "PRI", "VIR")),
+                           prediction_type="full_perc", expected_value=q.50, gt_source=gt_source) %>%
+             rename_at(vars(c("dates", "state", "fcst_date", paste0("q.", c("01", "025", "05", seq(10, 95, 5), "975", "99")))), 
+                       ~ c("target_end_date", "location_short", "forecast_date", paste0("perc_0.", formatC(c(10, 25, seq(50, 950, 50), 975, 990), width=3, flag="0")))) %>%
+             filter(lookahead>0)
+summary_us = summary_us[,c(1,29:31,27,32,33,3:25,34)]
 
-# Calculating various measure
-error  = all_merge$incident.death - all_merge$q.50
-pe     = error/all_merge$incident.death * 100
-adj_pe = error/(all_merge$incident.death + all_merge$q.50) * 100
-
-pe[all_merge$q.50==0 & all_merge$incident.death==0]     = 0
-adj_pe[all_merge$q.50==0 & all_merge$incident.death==0] = 0
-pe[all_merge$q.50!=0 & all_merge$incident.death==0]     = Inf
-
-ape              = abs(pe)
-adj_ape          = abs(adj_pe)
-logistic_ape     = 1 / (1 + exp(-ape/100))
-logistic_adj_ape = 1 / (1 + exp(-adj_ape/100))
-
-above = all_merge$incident.death > all_merge$q.975
-below = all_merge$incident.death < all_merge$q.025
-
-within_95_pi                             = rep(NA, nrow(all_merge))
-within_95_pi[which(above==F & below==F)] = "inside"
-within_95_pi[which(above==T)]            = "above"
-within_95_pi[which(below==T)]            = "below"
-
-outside_95p_by                             = rep(NA, nrow(all_merge))
-outside_95p_by[which(above==F & below==F)] = 0
-outside_95p_by[which(above==T)]            = all_merge$incident.death[which(above==T)] - all_merge$q.975[which(above==T)]
-outside_95p_by[which(below==T)]            = all_merge$incident.death[which(below==T)] - all_merge$q.025[which(below==T)]
-
-df = data.frame(target_date      = all_merge$dates, 
-                forecast_date    = all_merge$fcst_date, 
-                lookahead        = difftime(all_merge$dates, all_merge$fcst_date, units="days"),
-                model_name       = model[i],
-                location_long    = location_long,
-                location_short   = location_short,
-                prediction_type  = "full_perc",
-                expected_value   = all_merge$q.50,
-                perc_0.010       = all_merge$q.01,
-                perc_0.025       = all_merge$q.025,
-                perc_0.050       = all_merge$q.05,
-                perc_0.100       = all_merge$q.10,
-                perc_0.150       = all_merge$q.15,
-                perc_0.200       = all_merge$q.20,
-                perc_0.250       = all_merge$q.25,
-                perc_0.300       = all_merge$q.30,
-                perc_0.350       = all_merge$q.35,
-                perc_0.400       = all_merge$q.40,
-                perc_0.450       = all_merge$q.45,
-                perc_0.500       = all_merge$q.50,
-                perc_0.550       = all_merge$q.55,
-                perc_0.600       = all_merge$q.60,
-                perc_0.650       = all_merge$q.65,
-                perc_0.700       = all_merge$q.70,
-                perc_0.750       = all_merge$q.75,
-                perc_0.800       = all_merge$q.80,
-                perc_0.850       = all_merge$q.85,
-                perc_0.900       = all_merge$q.90,
-                perc_0.950       = all_merge$q.95,
-                perc_0.975       = all_merge$q.975,
-                perc_0.990       = all_merge$q.99,
-                gt_source        = gt_source,
-                gt_jhu           = all_merge$incident.death,
-                gt_nyt           = NA,
-                gt_ecdc          = NA,
-                error            = error,
-                pe               = pe,
-                adj_pe           = adj_pe,
-                ape              = ape,
-                adj_ape          = adj_ape,
-                logistic_ape     = logistic_ape,
-                logistic_adj_ape = logistic_adj_ape,
-                within_95_pi     = within_95_pi,
-                outside_95p_by   = outside_95p_by
-)
-
+# Save into summary folder
 setwd(wkdir)
-
-# Save data frame in summary folder
 files_in_dir = list.files()
-if("summary" %in% files_in_dir == F){
+if(!"summary" %in% files_in_dir){
   dir.create("summary")
 }
 setwd(paste0(wkdir, "/summary/"))
-
-write.csv(df, paste0(model[i], "-summary.csv"), row.names = F)
+print(paste("Saving prediction data for model", model, "in summary folder"))
+write.csv(rbind.fill(summary_global, summary_us), paste0(model, "_summary.csv"), row.names = F)
 setwd(wkdir)
-}
-rm(list = ls())
--- gt_downloader.R
+++ gt_downloader.R
@@ -1,15 +1,18 @@
 ###############################################################################################################
-# Script for downloading and standardising ground truth data from JHU, NYT and ECDC
+# Script for downloading and compiling ground truth data from JHU, NYT and ECDC
 ###############################################################################################################
 
 rm(list=ls())
 
 # Set working directory
-setwd("~/Code/covid-19")
+wkdir = "~/Code/covid-19"
+setwd(wkdir)
 
 # Installing required packages
-packages = c("dplyr", "downloader", "countrycode", "lubridate", "tidyr")
-install.packages(packages)
+packages = c("dplyr", "downloader", "countrycode", "lubridate", "plyr", "tidyr")
+if(!all(packages %in% rownames(installed.packages()))){
+  install.packages(packages[!packages %in% rownames(installed.packages())])
+}
 lapply(packages, require, character.only=T)
 
 # JHU global data
@@ -17,74 +20,107 @@ print("Downloading ground truth global data from JHU")
 download("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", "jhu_global.csv", quiet=T)
 jhu_global = read.csv("jhu_global.csv", stringsAsFactors=F, check.names=F)
 unlink("jhu_global.csv", recursive=T)
-jhu_global = jhu_global %>%
-             select(location_name=`Country/Region`, ends_with("/20")) %>%
-             group_by(location_name) %>%
-             summarise_all(list(~sum(.))) %>%
-             gather(date, cum.death, -location_name) %>%
-             group_by(location_name) %>%
-             mutate(date=mdy(date), inc.death=cum.death-lag(cum.death), gt_source="JHU") %>%
-             drop_na(inc.death) %>%
-             as.data.frame()
-print("Writing standardised ground truth global data from JHU")
-write.csv(jhu_global, "jhu_global.csv", row.names=F)
 
+# ECDC global data
+print("Downloading ground truth global data from ECDC")
+download("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", "ecdc_global.csv", quiet=T)
+ecdc_global = read.csv("ecdc_global.csv", stringsAsFactors=F, check.names=F)
+ecdc_global = ecdc_global %>%
+              select(location_long=countriesAndTerritories, date=dateRep, inc.death=deaths) %>%
+              dplyr::mutate(location_long=gsub("_", " ", location_long), date=as.Date(format(strptime(date, "%d/%m/%Y"), "%Y-%m-%d")))
+unlink("ecdc_global.csv", recursive=T)
 
 # JHU US states data
 print("Downloading ground truth US states data from JHU")
 download("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv", "jhu_us.csv", quiet=T)
 jhu_us = read.csv("jhu_us.csv", stringsAsFactors=F, check.names=F)
 unlink("jhu_us.csv", recursive=T)
-jhu_us = jhu_us %>%
-         select(location_name=Province_State, ends_with("/20")) %>%
-         group_by(location_name) %>%
-         summarise_all(list(~sum(.))) %>%
-         gather(date, cum.death, -location_name) %>%
-         group_by(location_name) %>%
-         mutate(date=mdy(date), inc.death=cum.death-lag(cum.death), gt_source="JHU") %>%
-         drop_na(inc.death) %>%
-         as.data.frame()
-print("Writing standardised ground truth US states data from JHU")
-write.csv(jhu_us, "jhu_us.csv", row.names=F)
 
-                   
 # NYT US data
 print("Downloading ground truth US data from NYT")
 download("https://github.com/nytimes/covid-19-data/raw/master/us.csv", "nyt_us.csv", quiet=T)
 nyt_us = read.csv("nyt_us.csv", stringsAsFactors=F, check.names=F)
 unlink("nyt_us.csv", recursive=T)
-nyt_us = nyt_us %>%
-         select(date, cum.death=deaths) %>%
-         mutate(inc.death=cum.death-lag(cum.death), location_name="US", gt_source="NYT") %>%
-         drop_na(inc.death)
 
 # NYT US states data
 print("Downloading ground truth US states data from NYT")
 download("https://github.com/nytimes/covid-19-data/raw/master/us-states.csv", "nyt_us_states.csv", quiet=T)
 nyt_us_states = read.csv("nyt_us_states.csv", stringsAsFactors=F, check.names=F)
 unlink("nyt_us_states.csv", recursive=T)
-nyt_us = nyt_us_states %>%
-         select(location_name=state, date, cum.death=deaths) %>%
-         group_by(location_name) %>%
-         mutate(date=as.Date(date), inc.death=cum.death-lag(cum.death), gt_source="NYT") %>%
+
+###############################################################################################################
+# Standardising country/US state name and add iso3c/state abb.
+###############################################################################################################
+
+# List name of countries in the global data and standardising name
+all_countries    = unique(c(jhu_global$`Country/Region`, ecdc_global$location_long))
+loc_short_global = countrycode(all_countries, origin="country.name", destination="iso3c", warn=F, nomatch=NULL)
+loc_long_global  = countrycode(loc_short_global, origin="iso3c", destination="country.name", warn=F, nomatch=NULL)
+loc_short_global[all_countries=="Kosovo"] = "KSV"
+
+# List of US states and sovereign
+all_us       = unique(c(jhu_us$Province_State, nyt_us_states$state))
+loc_long_us  = gsub("Virgin Islands", "U.S. Virgin Islands", all_us)
+loc_short_us = mapvalues(all_us, from=c(state.name, "District of Columbia", "American Samoa", "Guam", "Northern Mariana Islands", "Puerto Rico", "Virgin Islands"),
+                         to=c(state.abb, "DC", "ASM", "GUM", "MNP", "PRI", "VIR"))
+
+###############################################################################################################
+# Compiling all ground truth data
+###############################################################################################################
+jhu_global = jhu_global %>%
+             select(location_long=`Country/Region`, ends_with("/20")) %>%
+             dplyr::mutate(location_long=mapvalues(location_long, from=all_countries, to=loc_long_global, warn_missing=F)) %>%
+             group_by(location_long) %>%
+             summarise_all(list(~sum(.))) %>%
+             gather(date, cum.death, -location_long) %>%
+             group_by(location_long) %>%
+             dplyr::mutate(location_short=mapvalues(location_long, from=loc_long_global, to=loc_short_global, warn_missing=F), 
+                           date=mdy(date), inc.death=cum.death-lag(cum.death), gt_source="JHU") %>%
+             drop_na(inc.death) %>%
+             as.data.frame()
+
+jhu_us = jhu_us %>%
+         select(location_long=Province_State, ends_with("/20")) %>%
+         dplyr::mutate(location_long=mapvalues(location_long, from=all_us, to=loc_long_us, warn_missing=F)) %>%
+         group_by(location_long) %>%
+         summarise_all(list(~sum(.))) %>%
+         gather(date, cum.death, -location_long) %>%
+         group_by(location_long) %>%
+         dplyr::mutate(location_short=mapvalues(location_long, from=loc_long_us, to=loc_short_us, warn_missing=F), 
+                       date=mdy(date), inc.death=cum.death-lag(cum.death), gt_source="JHU") %>%
          drop_na(inc.death) %>%
-         as.data.frame() %>%
-         rbind(., nyt_us)
-print("Writing standardised ground truth US states data from NYT")
-write.csv(nyt_us, "nyt_us.csv", row.names=F)
+         as.data.frame()
 
+nyt_us = nyt_us %>%
+         select(date, cum.death=deaths) %>%
+         dplyr::mutate(date=as.Date(date), inc.death=cum.death-lag(cum.death), location_long="United States", location_short="USA", gt_source="NYT") %>%
+         drop_na(inc.death)
 
-# ECDC global data
-print("Downloading ground truth global data from ECDC")
-download("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", "ecdc_global.csv", quiet=T)
-ecdc_global = read.csv("ecdc_global.csv", stringsAsFactors=F, check.names=F)
-unlink("ecdc_global.csv", recursive=T)
+nyt_us_states = nyt_us_states %>%
+                select(location_long=state, date, cum.death=deaths) %>%
+                dplyr::mutate(location_long=mapvalues(location_long, from=all_us, to=loc_long_us, warn_missing=F)) %>%
+                group_by(location_long) %>%
+                dplyr::mutate(location_short=mapvalues(location_long, from=loc_long_us, to=loc_short_us, warn_missing=F),
+                              date=as.Date(date), inc.death=cum.death-lag(cum.death), gt_source="NYT") %>%
+                drop_na(inc.death) %>%
+                as.data.frame()
+
 ecdc_global = ecdc_global %>%
-              select(location_name=countriesAndTerritories, date=dateRep, inc.death=deaths) %>%
-              mutate(location_name=gsub("_", " ", location_name), date=as.Date(format(strptime(date, "%d/%m/%Y"), "%Y-%m-%d"))) %>%
-              group_by(location_name) %>%
+              dplyr::mutate(location_long=mapvalues(location_long, from=all_countries, to=loc_long_global, warn_missing=F)) %>%
+              group_by(location_long) %>%
               arrange(date) %>%
-              mutate(cum.death=cumsum(inc.death), gt_source="ECDC") %>%
+              dplyr::mutate(location_short=mapvalues(location_long, from=loc_long_global, to=loc_short_global, warn_missing=F), 
+                            cum.death=cumsum(inc.death), gt_source="ECDC") %>%
               as.data.frame()
-print("Writing standardised ground truth global data from ECDC")
-write.csv(ecdc_global, "ecdc_global.csv", row.names=F)
+
+gt = rbind.fill(jhu_global, jhu_us, nyt_us, nyt_us_states, ecdc_global) %>% select(date, location_long, location_short, gt_source, cum.death, inc.death)
+
+# Save into summary folder
+files_in_dir = list.files()
+if(!"summary" %in% files_in_dir){
+  dir.create("summary")
+}
+setwd(paste0(wkdir, "/summary/"))
+print("Saving ground truth data from JHU, NYT and ECDC in summary folder")
+write.csv(gt, "gt.csv", row.names=F)
+setwd(wkdir)
