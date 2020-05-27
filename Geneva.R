cce2d424ad06767d4372c40465f64f1c0b341fec
vchi2333 <vincent.chin@sydney.edu.au>
2020-05-27 11:36:39 +1000

    fix bugs

--- Geneva.R
+++ Geneva.R
@@ -10,16 +10,19 @@ setwd(paste0(wkdir, "/", model))
 gt_source = "ECDC"
 
 # Load csv files for the model, combine and convert into standardised format
-summary       = lapply(list.files(pattern=paste0("^", model, "(.*)csv$")), function(i) read.csv(i, stringsAsFactors=F, check.names=T))
-forecast_date = rep(str_extract(list.files(pattern=paste0("^", model, "(.*)csv$")), "[0-2]{4}[-][0-9]{2}[-][0-9]{2}"), unlist(lapply(summary, nrow)))
-summary       = cbind(forecast_date=forecast_date, do.call(rbind.fill, summary))
+summary     = lapply(list.files(pattern=paste0("^", model, "(.*)csv$")), function(i) read.csv(i, stringsAsFactors=F, check.names=T))
+report_date = rep(str_extract(list.files(pattern=paste0("^", model, "(.*)csv$")), "[0-2]{4}[-][0-9]{2}[-][0-9]{2}"), unlist(lapply(summary, nrow)))
+summary     = cbind(report_date=report_date, do.call(rbind.fill, summary))
 
-summary = summary %>%
-          filter(observed=="Predicted", is.finite(per.day), !is.na(per.day)) %>%
-          select(target_end_date=date, forecast_date=forecast_date, location_short=country, expected_value=per.day) %>%
-          dplyr::mutate(target_end_date=as.Date(target_end_date), forecast_date=as.Date(forecast_date),
-                 forecast_date=case_when(forecast_date==target_end_date ~ forecast_date-1, forecast_date<target_end_date ~ forecast_date),
-                 lookahead=target_end_date-forecast_date, model_name=model, prediction_type="point_estimate", gt_source=gt_source,
+summary = summary %>% 
+          mutate(date=as.Date(date)) %>%
+          group_by(report_date, country) %>%
+          summarise(forecast_date=max(date[observed=="Observed"])) %>%
+          as.data.frame() %>%
+          right_join(., summary, by=c("report_date", "country")) %>%
+          filter(observed=="Predicted", is.finite(per.day), !is.na(per.day), per.day>=0) %>%
+          select(target_end_date=date, forecast_date, location_short=country, expected_value=per.day) %>%
+          dplyr::mutate(target_end_date=as.Date(target_end_date), lookahead=target_end_date-forecast_date, model_name=model, prediction_type="point_estimate", gt_source=gt_source,
                  location_short=countrycode(gsub("CuraÃ§ao", "Curaçao", location_short), origin="country.name", destination="iso3c", nomatch=NULL),
                  location_short=gsub("Kosovo", "KSV", location_short), perc_0.010=NA, perc_0.025=NA, perc_0.050=NA, perc_0.100=NA, perc_0.150=NA, 
                  perc_0.200=NA, perc_0.250=NA, perc_0.300=NA, perc_0.350=NA, perc_0.400=NA, perc_0.450=NA, perc_0.500=NA, perc_0.550=NA, 
